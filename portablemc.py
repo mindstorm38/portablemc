64b96dc25e388aa25f2538c5db2744d211010af0
Théo Rozier <contact@theorozier.fr>
2021-03-25 09:19:42 +0100

    Version 1.1.1, important fix for standard package.

--- portablemc.py
+++ portablemc.py
@@ -28,7 +28,7 @@ import os
 
 
 LAUNCHER_NAME = "portablemc"
-LAUNCHER_VERSION = "1.1.0"
+LAUNCHER_VERSION = "1.1.1"
 LAUNCHER_AUTHORS = "Théo Rozier"
 
 VERSION_MANIFEST_URL = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
