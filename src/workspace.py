e4fbb9036baa8c020dd37e746035d27ec38f1972
Theo Rozier <contact@theorozier.fr>
2022-05-08 15:24:39 +0200

    Changed workspace script to execute on "core" before other modules.

--- src/workspace.py
+++ src/workspace.py
@@ -8,11 +8,17 @@ import sys
 import os
 
 def for_each_module(args: list[str]):
+
+    def inner(name: str, path: str):
+        print(f"{name} > {' '.join(args)}")
+        subprocess.call(args, cwd=path)
+
+    inner("core", "core")
+
     with os.scandir() as dirs:
         for entry in dirs:
-            if entry.is_dir():
-                print(f"{entry.name} > {' '.join(args)}")
-                subprocess.call(args, cwd=entry.path)
+            if entry.is_dir() and entry.name != "core":
+                inner(entry.name, entry.path)
 
 if __name__ == '__main__':
     for_each_module(["poetry"] + sys.argv[1:])
