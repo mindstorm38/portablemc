4748dbda69f337074981787492548d16bb0a3684
theor <contact@theorozier.fr>
2022-05-10 00:06:46 +0200

    Improved Fabric add-on for the new API. Now provides a little API.

--- src/fabric/portablemc_fabric/__init__.py
+++ src/fabric/portablemc_fabric/__init__.py
@@ -1,14 +1,9 @@
 from argparse import ArgumentParser, Namespace
 from datetime import datetime
 from typing import Optional
-from os import path
-import json
 import sys
-import os
 
-from portablemc import Version, VersionManifest, BaseError
-from portablemc import cli as pmc
-from portablemc.cli import CliContext
+from portablemc import Version, BaseError, Context, json_simple_request
 
 
 FABRIC_META_URL = "https://meta.fabricmc.net/{}"
@@ -17,6 +12,9 @@ FABRIC_VERSIONS_LOADER_VERSIONED = "v2/versions/loader/{}/{}"
 
 
 def load():
+
+    from portablemc.cli import CliContext
+    from portablemc import cli as pmc
 
     # Private mixins
 
@@ -39,8 +37,6 @@ def load():
 
     @pmc.mixin()
     def new_version(old, ctx: CliContext, version_id: str) -> Version:
-
-        # TODO: Return a real sub-class of Version just for handling Fabric.
 
         if version_id.startswith("fabric:"):
 
@@ -65,68 +61,85 @@ def load():
                 loader_meta = request_version_loader(game_version, None)
                 loader_version = loader_meta["loader"]["version"]
             else:
-                pmc.print_task("", "start.fabric.resolving_loader_with_version", {
-                    "loader_version": loader_version,
-                    "game_version": game_version
-                })
-                # Loader meta is none because if the 'loader_version' is set, we do not need it to check if dir exists.
                 loader_meta = None
-
-            version_id = f"{ctx.ns.fabric_prefix}-{game_version}-{loader_version}"
-            version_dir = ctx.get_version_dir(version_id)
-            version_meta_file = path.join(version_dir, f"{version_id}.json")
-
-            if not path.isdir(version_dir) or not path.isfile(version_meta_file):
-
-                pmc.print_task("", "start.fabric.generating")
-
-                if loader_meta is None:
-                    # If the directory does not exist and the loader_version was provided, request meta.
-                    loader_meta = request_version_loader(game_version, loader_version)
-
-                loader_launcher_meta = loader_meta["launcherMeta"]
-
-                iso_time = datetime.now().isoformat()
-
-                version_libraries = loader_launcher_meta["libraries"]["common"]
-                version_meta = {
-                    "id": version_id,
-                    "inheritsFrom": game_version,
-                    "releaseTime": iso_time,
-                    "time": iso_time,
-                    "type": get_version_type(manifest, game_version),
-                    "mainClass": loader_launcher_meta["mainClass"]["client"],
-                    "arguments": {
-                        "game": [],
-                        "jvm": [
-                            # TODO: Might add "-DFabricMcEmu= net.minecraft.client.main.Main " in the future.
-                        ]
-                    },
-                    "libraries": version_libraries
-                }
-
-                version_libraries.append({
-                    "name": loader_meta["loader"]["maven"],
-                    "url": "https://maven.fabricmc.net/"
-                })
-
-                version_libraries.append({
-                    "name": loader_meta["intermediary"]["maven"],
-                    "url": "https://maven.fabricmc.net/"
-                })
-
-                os.makedirs(version_dir, exist_ok=True)
-                with open(version_meta_file, "wt") as fp:
-                    json.dump(version_meta, fp, indent=2)
 
             pmc.print_task("OK", "start.fabric.resolved_loader", {
                 "loader_version": loader_version,
                 "game_version": game_version
             }, done=True)
 
-            version = Version(ctx, version_id)
-            version.manifest = manifest
-            return version
+            ver = FabricVersion(ctx, game_version, loader_version, prefix=ctx.ns.fabric_prefix)
+            ver.manifest = manifest
+            ver.loader_meta = loader_meta
+            return ver
+
+            # if loader_version is None:
+            #     pmc.print_task("", "start.fabric.resolving_loader", {"game_version": game_version})
+            #     loader_meta = request_version_loader(game_version, None)
+            #     loader_version = loader_meta["loader"]["version"]
+            # else:
+            #     pmc.print_task("", "start.fabric.resolving_loader_with_version", {
+            #         "loader_version": loader_version,
+            #         "game_version": game_version
+            #     })
+            #     # Loader meta is none because if the 'loader_version' is set, we do not need it to check if dir exists.
+            #     loader_meta = None
+            #
+            # version_id = f"{ctx.ns.fabric_prefix}-{game_version}-{loader_version}"
+            # version_dir = ctx.get_version_dir(version_id)
+            # version_meta_file = path.join(version_dir, f"{version_id}.json")
+            #
+            # if not path.isdir(version_dir) or not path.isfile(version_meta_file):
+            #
+            #     pmc.print_task("", "start.fabric.generating")
+            #
+            #     if loader_meta is None:
+            #         # If the directory does not exist and the loader_version was provided, request meta.
+            #         loader_meta = request_version_loader(game_version, loader_version)
+            #
+            #     loader_launcher_meta = loader_meta["launcherMeta"]
+            #
+            #     iso_time = datetime.now().isoformat()
+            #
+            #     version_libraries = loader_launcher_meta["libraries"]["common"]
+            #     version_meta = {
+            #         "id": version_id,
+            #         "inheritsFrom": game_version,
+            #         "releaseTime": iso_time,
+            #         "time": iso_time,
+            #         "type": manifest.get_version_type(game_version),
+            #         "mainClass": loader_launcher_meta["mainClass"]["client"],
+            #         "arguments": {
+            #             "game": [],
+            #             "jvm": [
+            #                 # TODO: Might add "-DFabricMcEmu= net.minecraft.client.main.Main " in the future.
+            #             ]
+            #         },
+            #         "libraries": version_libraries
+            #     }
+            #
+            #     version_libraries.append({
+            #         "name": loader_meta["loader"]["maven"],
+            #         "url": "https://maven.fabricmc.net/"
+            #     })
+            #
+            #     version_libraries.append({
+            #         "name": loader_meta["intermediary"]["maven"],
+            #         "url": "https://maven.fabricmc.net/"
+            #     })
+            #
+            #     os.makedirs(version_dir, exist_ok=True)
+            #     with open(version_meta_file, "wt") as fp:
+            #         json.dump(version_meta, fp, indent=2)
+            #
+            # pmc.print_task("OK", "start.fabric.resolved_loader", {
+            #     "loader_version": loader_version,
+            #     "game_version": game_version
+            # }, done=True)
+            #
+            # version = Version(ctx, version_id)
+            # version.manifest = manifest
+            # return version
 
         return old(ctx, version_id)
 
@@ -135,19 +148,75 @@ def load():
     pmc.messages.update({
         "args.start.fabric_prefix": "Change the prefix of the version ID when starting with Fabric.",
         "start.fabric.resolving_loader": "Resolving fabric loader for {game_version}...",
-        "start.fabric.resolving_loader_with_version": "Resolving fabric loader {loader_version} for {game_version}...",
+        # "start.fabric.resolving_loader_with_version": "Resolving fabric loader {loader_version} for {game_version}...",
         "start.fabric.resolved_loader": "Resolved fabric loader {loader_version} for {game_version}.",
-        "start.fabric.generating": "Generating fabric version meta...",
+        # "start.fabric.generating": "Generating fabric version meta...",
         "start.fabric.error.invalid_format": "To launch fabric, use 'fabric:[<mc-version>[:<loader-version>]]'.",
         f"start.fabric.error.{FabricVersionNotFound.GAME_VERSION_NOT_FOUND}": "Game version {version} not found.",
         f"start.fabric.error.{FabricVersionNotFound.LOADER_VERSION_NOT_FOUND}": "Loader version {version} not found."
     })
 
 
+class FabricVersion(Version):
+
+    def __init__(self, context: Context, game_version: str, loader_version: str, *, prefix: str = "fabric"):
+        super().__init__(context, f"{prefix}-{game_version}-{loader_version}")
+        self.game_version = game_version
+        self.loader_version = loader_version
+        self.loader_meta: Optional[dict] = None
+
+    def _validate_version_meta(self, version_id: str, version_dir: str, version_meta_file: str, version_meta: dict) -> bool:
+        if version_id == self.id:
+            return True
+        else:
+            return super()._validate_version_meta(version_id, version_dir, version_meta_file, version_meta)
+
+    def _fetch_version_meta(self, version_id: str, version_dir: str, version_meta_file: str) -> dict:
+
+        if version_id != self.id:
+            return super()._fetch_version_meta(version_id, version_dir, version_meta_file)
+
+        if self.loader_meta is None:
+            # If the directory does not exist and the loader_version was provided, request meta.
+            self.loader_meta = request_version_loader(self.game_version, self.loader_version)
+
+        loader_launcher_meta = self.loader_meta["launcherMeta"]
+
+        iso_time = datetime.now().isoformat()
+
+        version_libraries = loader_launcher_meta["libraries"]["common"]
+
+        version_libraries.append({
+            "name": self.loader_meta["loader"]["maven"],
+            "url": "https://maven.fabricmc.net/"
+        })
+
+        version_libraries.append({
+            "name": self.loader_meta["intermediary"]["maven"],
+            "url": "https://maven.fabricmc.net/"
+        })
+
+        return {
+            "id": version_id,
+            "inheritsFrom": self.game_version,
+            "releaseTime": iso_time,
+            "time": iso_time,
+            "type": self._ensure_version_manifest().get_version_type(self.game_version),
+            "mainClass": loader_launcher_meta["mainClass"]["client"],
+            "arguments": {
+                "game": [],
+                "jvm": [
+                    # TODO: Might add "-DFabricMcEmu= net.minecraft.client.main.Main " in the future.
+                ]
+            },
+            "libraries": version_libraries
+        }
+
+
 # FabricMC API
 
 def request_meta(method: str) -> dict:
-    return pmc.json_simple_request(FABRIC_META_URL.format(method), ignore_error=True)
+    return json_simple_request(FABRIC_META_URL.format(method), ignore_error=True)
 
 
 def request_version_loader(game_version: str, loader_version: Optional[str]) -> Optional[dict]:
@@ -163,11 +232,6 @@ def request_version_loader(game_version: str, loader_version: Optional[str]) -> 
                 raise FabricVersionNotFound(FabricVersionNotFound.GAME_VERSION_NOT_FOUND, game_version)
             raise FabricVersionNotFound(FabricVersionNotFound.LOADER_VERSION_NOT_FOUND, loader_version)
         return ret
-
-
-def get_version_type(manifest: VersionManifest, version: str) -> str:
-    version_obj = manifest.get_version(version)
-    return "release" if version_obj is None else version_obj.get("type", "release")
 
 
 # Errors
