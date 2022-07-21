394f930240f94f45c354bbcc15cc1a920051771f
theor <contact@theorozier.fr>
2022-07-21 23:52:09 +0200

    Improved file deletion when download fails for not found/connection lost cases.

--- test/core/test_download.py
+++ test/core/test_download.py
@@ -48,7 +48,42 @@ def test_download(tmp_path):
                              size=3665,
                              name="conn_err")
 
+    sacrificial0 = DownloadEntry("https://resources.download.minecraft.net/bd/bdf48ef6b5d0d23bbb02e17d04865216179f510a",
+                             str(assets_dir / "icons" / "icon_16x16_sacrificial0.png"),
+                             name="sacrificial0")
+
+    sacrificial1 = DownloadEntry("https://resources.download.minecraft.net/bd/bdf48ef6b5d0d23bbb02e17d04865216179f510a",
+                             str(assets_dir / "icons" / "icon_16x16_sacrificial1.png"),
+                             name="sacrificial1")
+
+    # This error should have overwritten the file
+    wrong_sha1_sacrificial0 = DownloadEntry("https://resources.download.minecraft.net/bd/bdf48ef6b5d0d23bbb02e17d04865216179f510a",
+                             str(assets_dir / "icons" / "icon_16x16_sacrificial0.png"),
+                             sha1="bdf48ef6b5d0d23bbb02e17d04865216179f510b",
+                             name="wrong_sha1_sacrificial0")
+
+    # This error should have overwritten the file
+    wrong_size_sacrificial1 = DownloadEntry("https://resources.download.minecraft.net/bd/bdf48ef6b5d0d23bbb02e17d04865216179f510a",
+                               str(assets_dir / "icons" / "icon_16x16_sacrificial1.png"),
+                               size=1189,
+                               name="wrong_size_sacrificial1")
+
+    not_sacrificial = DownloadEntry("https://resources.download.minecraft.net/bd/bdf48ef6b5d0d23bbb02e17d04865216179f510a",
+                             str(assets_dir / "icons" / "icon_16x16_not_sacrificial.png"),
+                             name="not_sacrificial")
+
+    # This error should not overwrite de file
+    not_found_not_sacrificial = DownloadEntry("https://resources.download.minecraft.net/bd/bdf48ef6b5d0d23bbb02e17d04865216",
+                             str(assets_dir / "icons" / "icon_16x16_not_sacrificial.png"),
+                             name="not_found_not_sacrificial")
+
+    # This error should not overwrite de file
+    conn_err_not_sacrificial = DownloadEntry("https://rfdfdfesources.download.minecraft.net/bd/bdf48ef6b5d0d23bbb02e17d04865216",
+                             str(assets_dir / "icons" / "icon_16x16_not_sacrificial.png"),
+                             name="conn_err_not_sacrificial")
+
     dl = DownloadList()
+
     dl.append(default)
     dl.append(check_sha1)
     dl.append(check_size)
@@ -57,19 +92,34 @@ def test_download(tmp_path):
     dl.append(wrong_size)
     dl.append(not_found)
     dl.append(conn_err)
+
+    dl.append(sacrificial0)
+    dl.append(sacrificial1)
+    dl.append(wrong_sha1_sacrificial0)
+    dl.append(wrong_size_sacrificial1)
+
+    dl.append(not_sacrificial)
+    dl.append(not_found_not_sacrificial)
+    dl.append(conn_err_not_sacrificial)
+
     report = dl.download_files()
+
+    print(report.fails)
 
     assert report.fails[wrong_sha1] == DownloadReport.INVALID_SHA1
     assert report.fails[wrong_size] == DownloadReport.INVALID_SIZE
     assert report.fails[not_found] == DownloadReport.NOT_FOUND
     assert report.fails[conn_err] == DownloadReport.CONN_ERROR
-    assert len(report.fails) == 4
+    assert len(report.fails) == 8
 
     assert path.isfile(default.dst)
     assert path.isfile(check_sha1.dst)
     assert path.isfile(check_size.dst)
     assert path.isfile(check_all.dst)
+    assert path.isfile(not_sacrificial.dst)
     assert not path.isfile(wrong_sha1.dst)
     assert not path.isfile(wrong_size.dst)
     assert not path.isfile(not_found.dst)
     assert not path.isfile(conn_err.dst)
+    assert not path.isfile(sacrificial0.dst)
+    assert not path.isfile(sacrificial1.dst)
