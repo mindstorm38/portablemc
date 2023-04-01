7ca0906cca5a771582d8e7809b3e31fae0c829e0
Theo Rozier <contact@theorozier.fr>
2023-04-01 15:49:27 +0200

    Version 3.3.1

--- src/core/portablemc/__init__.py
+++ src/core/portablemc/__init__.py
@@ -61,7 +61,7 @@ __all__ = [
 
 
 LAUNCHER_NAME = "portablemc"
-LAUNCHER_VERSION = "3.3.0"
+LAUNCHER_VERSION = "3.3.1"
 LAUNCHER_AUTHORS = ["Théo Rozier <contact@theorozier.fr>", "Github contributors"]
 LAUNCHER_COPYRIGHT = "PortableMC  Copyright (C) 2021-2023  Théo Rozier"
 LAUNCHER_URL = "https://github.com/mindstorm38/portablemc"
