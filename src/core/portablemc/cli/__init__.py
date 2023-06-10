738aeee92aa6273531418d6a79c1ad6883bdf9eb
Theo Rozier <contact@theorozier.fr>
2023-06-10 12:44:53 +0200

    Working on CLI (search command in manifest is working)

--- src/core/portablemc/cli/__init__.py
+++ src/core/portablemc/cli/__init__.py
@@ -4,14 +4,18 @@
 from argparse import Namespace
 import sys
 
-from .output import Output, HumanOutput
-from .cmd import register_arguments
+from .parse import register_arguments
+from .util import format_locale_date
+from .output import Table
 from .lang import get as _
 
-from ..manifest import VersionManifest
+from ..manifest import VersionManifest, VersionManifestError
 from ..standard import Context
 
-from typing import Optional, List, Union, Dict, Callable, Any
+from typing import TYPE_CHECKING, Optional, List, Union, Dict, Callable, Any
+
+if TYPE_CHECKING:
+    from .parse import RootCmd, SearchCmd
 
 
 EXIT_OK = 0
@@ -27,8 +31,6 @@ def main(args: Optional[List[str]] = None):
     parser = register_arguments()
     ns = parser.parse_args(args or sys.argv[1:])
 
-    cli = Cli(ns, HumanOutput())
-
     command_handlers = get_command_handlers()
     command_attr = "subcommand"
     while True:
@@ -38,7 +40,7 @@ def main(args: Optional[List[str]] = None):
             parser.print_help()
             sys.exit(EXIT_FAILURE)
         elif callable(handler):
-            handler(cli)
+            handler(ns)
         elif isinstance(handler, dict):
             command_attr = f"{command}_{command_attr}"
             command_handlers = handler
@@ -46,18 +48,8 @@ def main(args: Optional[List[str]] = None):
         sys.exit(EXIT_OK)
 
 
-CommandFunc = Callable[[Cli], Any]
+CommandFunc = Callable[[Namespace], Any]
 CommandDict = Dict[str, Union[CommandFunc, "CommandDict"]]
-
-
-class Cli:
-    """A bundle of runtime properties for the CLI. This contains the namespace of parsed
-    CLI arguments with the output handle used to print things to the user.
-    """
-
-    def __init__(self, ns: Namespace, out: Output) -> None:
-        self.ns = ns
-        self.out = out
 
 
 def get_command_handlers() -> CommandDict:
@@ -82,94 +74,61 @@ def get_command_handlers() -> CommandDict:
     }
 
 
-def cmd_search(cli: Cli):
+def cmd_search(ns: "SearchCmd"):
 
-    table = []
-    search = cli.ns.input
-    no_version = search is None
+    def cmd_search_manifest(search: Optional[str], table: Table):
+        
+        table.add(
+            _("search.type"),
+            _("search.name"),
+            _("search.release_date"),
+            _("search.flags"))
+        table.separator()
 
-    if cli.ns.local:
-        raise NotImplementedError
-        # for version_id, mtime in ctx.list_versions():
-        #     if no_version or search in version_id:
-        #         table.append((version_id, format_locale_date(mtime)))
-    else:
-
-        manifest = VersionManifest()
-
-        manifest = new_version_manifest(ctx)
+        manifest = new_version_manifest(ns)
         search, alias = manifest.filter_latest(search)
         try:
             for version_data in manifest.all_versions():
                 version_id = version_data["id"]
-                if no_version or (alias and search == version_id) or (not alias and search in version_id):
-                    table.append((
-                        version_data["type"],
-                        version_id,
+                if search is None or (alias and search == version_id) or (not alias and search in version_id):
+                    table.add(
+                        version_data["type"], 
+                        version_id, 
                         format_locale_date(version_data["releaseTime"]),
-                        _("search.flags.local") if ctx.has_version_metadata(version_id) else ""
-                    ))
+                        "")
+
+                    # table.append((
+                    #     version_data["type"],
+                    #     version_id,
+                    #     format_locale_date(version_data["releaseTime"]),
+                    #     _("search.flags.local") if ctx.has_version_metadata(version_id) else ""
+                    # ))
+
         except VersionManifestError as err:
-            print_task("FAILED", f"version_manifest.error.{err.code}", done=True)
-            sys.exit(EXIT_VERSION_NOT_FOUND)
+            pass
+            # print_task("FAILED", f"version_manifest.error.{err.code}", done=True)
+            # sys.exit(EXIT_VERSION_NOT_FOUND)
 
-    if len(table):
-        table.insert(0, (
+    def cmd_search_local(search: Optional[str], table: Table):
+
+        table.add(
             _("search.name"),
-            _("search.last_modified")
-        ) if ns.local else (
-            _("search.type"),
-            _("search.name"),
-            _("search.release_date"),
-            _("search.flags")
-        ))
-        print_table(table, header=0)
-        sys.exit(EXIT_OK)
-    else:
-        print_message("search.not_found")
-        sys.exit(EXIT_VERSION_NOT_FOUND)
+            _("search.last_modified"))
+        table.separator()
+
+        raise NotImplementedError
+
+    search_handler = {
+        "manifest": cmd_search_manifest,
+        "local": cmd_search_local
+    }[ns.kind]
+
+    table = ns.out.table()
+    search_handler(ns.input, table)
+
+    table.print()
+    sys.exit(EXIT_OK)
 
 
-# def cmd_start(ns: Namespace, out: Output):
-    
-#     from portablemc.standard import make_standard_sequence
-
-#     seq = make_standard_sequence()
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def cmd(handler: CommandHandler, ns: Namespace):
-#     try:
-#         handler(ns)
-#     except JsonRequestError as err:
-#         print_task("FAILED", f"json_request.error.{err.code}", {
-#             "url": err.url,
-#             "method": err.method,
-#             "status": err.status,
-#             "data": err.data,
-#         }, done=True, keep_previous=True)
-#         sys.exit(EXIT_JSON_REQUEST_ERROR)
-#     except KeyboardInterrupt:
-#         print_task(None, "error.keyboard_interrupt", done=True, keep_previous=True)
-#         sys.exit(EXIT_FAILURE)
-#     except Exception as err:
-#         import ssl
-#         key = "error.generic"
-#         if isinstance(err, URLError) and isinstance(err.reason, ssl.SSLCertVerificationError):
-#             key = "error.cert"
-#         elif isinstance(err, (URLError, socket.gaierror, socket.timeout)):
-#             key = "error.socket"
-#         print_task("FAILED", key, done=True, keep_previous=True)
-#         import traceback
-#         traceback.print_exc()
-#         sys.exit(EXIT_FAILURE)
+def new_version_manifest(ns: "RootCmd") -> VersionManifest:
+    return VersionManifest()
---
+++ src/core/portablemc/cli/parse.py
@@ -0,0 +1,170 @@
+from argparse import ArgumentParser, HelpFormatter, Namespace
+
+from .output import Output, HumanOutput, JsonOutput
+from .util import LibrarySpecifierFilter
+from .lang import get as _
+
+from typing import TYPE_CHECKING, Optional, Type, Tuple, List
+
+
+if TYPE_CHECKING:
+
+    class RootCmd(Namespace):
+        main_dir: str
+        work_dir: str
+        timeout: float
+        out: Output
+    
+    class SearchCmd(RootCmd):
+        kind: str
+        input: str
+
+    class StartCmd(RootCmd):
+        dry: bool
+        disable_mp: bool
+        disable_chat: bool
+        demo: bool
+        resolution: Optional[Tuple[int, int]]
+        jvm: Optional[str]
+        jvm_args: Optional[str]
+        no_better_logging: bool
+        anonymize: bool
+        no_legacy_fix: bool
+        lwjgl: Optional[str]
+        exclude_lib: Optional[List[LibrarySpecifierFilter]]
+        include_bin: Optional[List[str]]
+        temp_login: bool
+        login: str
+        login_service: str
+        username: Optional[str]
+        uuid: Optional[str]
+        server: Optional[str]
+        server_port: Optional[int]
+        version: str
+
+    class LoginCmd(RootCmd):
+        login_service: str
+        email_or_username: str
+
+    class LogoutCmd(RootCmd):
+        login_service: str
+        email_or_username: str
+    
+    class ShowCmd(RootCmd):
+        pass
+
+    class ShowAboutCmd(ShowCmd):
+        pass
+
+    class ShowAuthCmd(ShowCmd):
+        pass
+
+    class ShowLangCmd(ShowCmd):
+        pass
+
+
+def register_arguments() -> ArgumentParser:
+    parser = ArgumentParser(allow_abbrev=False, prog="portablemc", description=_("args"))
+    parser.add_argument("--main-dir", help=_("args.main_dir"))
+    parser.add_argument("--work-dir", help=_("args.work_dir"))
+    parser.add_argument("--timeout", help=_("args.timeout"), type=float)
+    parser.add_argument("--output", help=_("args.output"), dest="out", type=get_output_from_str, default="human", choices=["human", "json"])
+    register_subcommands(parser.add_subparsers(title="subcommands", dest="subcommand"))
+    return parser
+
+
+def register_subcommands(subparsers):
+    register_search_arguments(subparsers.add_parser("search", help=_("args.search")))
+    register_start_arguments(subparsers.add_parser("start", help=_("args.start")))
+    register_login_arguments(subparsers.add_parser("login", help=_("args.login")))
+    register_logout_arguments(subparsers.add_parser("logout", help=_("args.logout")))
+    register_show_arguments(subparsers.add_parser("show", help=_("args.show")))
+    register_addon_arguments(subparsers.add_parser("addon", help=_("args.addon")))
+
+
+def register_search_arguments(parser: ArgumentParser):
+    parser.add_argument("-k", "--kind", help=_("args.search.kind"), default="manifest", choices=["manifest", "local"])
+    parser.add_argument("input", nargs="?")
+
+
+def register_common_login_service(parser: ArgumentParser):
+    parser.add_argument("--login-service", help=_("args.common.login_service"), default="microsoft", choices=["microsoft", "mojang"])
+
+
+def register_start_arguments(parser: ArgumentParser):
+
+    def resolution(raw: str) -> Tuple[int, int]:
+        parts = raw.split("x")
+        if len(parts) == 2:
+            return (int(parts[0]), int(parts[1]))
+        else:
+            raise ValueError("Expected format: <width>x<height>")
+    
+    parser.formatter_class = new_help_formatter_class(32)
+    parser.add_argument("--dry", help=_("args.start.dry"), action="store_true")
+    parser.add_argument("--disable-mp", help=_("args.start.disable_multiplayer"), action="store_true")
+    parser.add_argument("--disable-chat", help=_("args.start.disable_chat"), action="store_true")
+    parser.add_argument("--demo", help=_("args.start.demo"), action="store_true")
+    parser.add_argument("--resolution", help=_("args.start.resolution"), type=resolution)
+    parser.add_argument("--jvm", help=_("args.start.jvm"))
+    parser.add_argument("--jvm-args", help=_("args.start.jvm_args"))
+    parser.add_argument("--no-better-logging", help=_("args.start.no_better_logging"), action="store_true")
+    parser.add_argument("--anonymize", help=_("args.start.anonymize"), action="store_true")
+    parser.add_argument("--no-legacy-fix", help=_("args.start.no_legacy_fix"), action="store_true")
+    parser.add_argument("--lwjgl", help=_("args.start.lwjgl"), choices=["3.2.3", "3.3.0", "3.3.1"])
+    parser.add_argument("--exclude-lib", help=_("args.start.exclude_lib"), action="append", type=LibrarySpecifierFilter.from_str)
+    parser.add_argument("--include-bin", help=_("args.start.include_bin"), action="append")
+    register_common_login_service(parser)
+    parser.add_argument("-t", "--temp-login", help=_("args.start.temp_login"), action="store_true")
+    parser.add_argument("-l", "--login", help=_("args.start.login"))
+    parser.add_argument("-u", "--username", help=_("args.start.username"), metavar="NAME")
+    parser.add_argument("-i", "--uuid", help=_("args.start.uuid"))
+    parser.add_argument("-s", "--server", help=_("args.start.server"))
+    parser.add_argument("-p", "--server-port", type=int, help=_("args.start.server_port"), metavar="PORT")
+    parser.add_argument("version", nargs="?", default="release")
+
+
+def register_login_arguments(parser: ArgumentParser):
+    register_common_login_service(parser)
+    parser.add_argument("email_or_username")
+
+
+def register_logout_arguments(parser: ArgumentParser):
+    register_common_login_service(parser)
+    parser.add_argument("email_or_username")
+
+
+def register_show_arguments(parser: ArgumentParser):
+    subparsers = parser.add_subparsers(title="subcommands", dest="show_subcommand")
+    subparsers.required = True
+    subparsers.add_parser("about", help=_("args.show.about"))
+    subparsers.add_parser("auth", help=_("args.show.auth"))
+    subparsers.add_parser("lang", help=_("args.show.lang"))
+
+
+def register_addon_arguments(parser: ArgumentParser):
+    subparsers = parser.add_subparsers(title="subcommands", dest="addon_subcommand")
+    subparsers.required = True
+    subparsers.add_parser("list", help=_("args.addon.list"))
+    show_parser = subparsers.add_parser("show", help=_("args.addon.show"))
+    show_parser.add_argument("addon_id")
+
+
+def new_help_formatter_class(max_help_position: int) -> Type[HelpFormatter]:
+
+    class CustomHelpFormatter(HelpFormatter):
+        def __init__(self, prog):
+            super().__init__(prog, max_help_position=max_help_position)
+
+    return CustomHelpFormatter
+
+
+def get_output_from_str(s: str) -> Output:
+    """This function is used to parse the output argument given.
+    """
+    if s == "human":
+        return HumanOutput()
+    elif s == "json":
+        return JsonOutput();
+    else:
+        raise ValueError(f"invalid type: {s}")
