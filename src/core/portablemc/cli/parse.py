ad520ea2e1e4c95abadd6eec72da987dd60bb625
Theo Rozier <contact@theorozier.fr>
2023-07-22 15:46:22 +0200

    Removed forge-all search kind

--- src/core/portablemc/cli/parse.py
+++ src/core/portablemc/cli/parse.py
@@ -162,7 +162,7 @@ def get_outputs() -> List[str]:
 
 
 def get_search_kinds() -> List[str]:
-    return ["manifest", "local", "forge", "forge-all"]
+    return ["manifest", "local", "forge"]
 
 
 def get_auth_services() -> List[str]:
