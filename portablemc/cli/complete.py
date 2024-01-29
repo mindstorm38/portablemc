eb22063135e964a0e61ebfe4fe6e01c20cf4846c
Théo Rozier <contact@theorozier.fr>
2024-01-29 12:19:15 +0100

    Added auto-loaded Zsh completion by default

--- portablemc/cli/complete.py
+++ portablemc/cli/complete.py
@@ -10,10 +10,15 @@ from .parse import type_path, type_path_dir, \
 from typing import Dict, Tuple, cast
 
 
-def gen_zsh_completion(parser: ArgumentParser) -> str:
+def gen_zsh_completion(parser: ArgumentParser, evaluated: bool) -> str:
     buffer = StringIO()
+    if not evaluated:
+        buffer.write("#compdef portablemc\n\n")
     gen_zsh_parser_completion(parser, buffer, "_portablemc")
-    buffer.write("compdef _portablemc portablemc\n")
+    if not evaluated:
+        buffer.write("_portablemc\n")
+    else:
+        buffer.write("compdef _portablemc portablemc\n")
     return buffer.getvalue()
 
 def gen_zsh_parser_completion(parser: ArgumentParser, buffer: StringIO, function: str):
