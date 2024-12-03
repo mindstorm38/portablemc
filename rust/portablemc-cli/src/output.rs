972c330b8d7e859551dcb10ebd41ae5875a056d3
Theo Rozier <contact@theorozier.fr>
2024-12-03 14:25:46 +0100

    Working on table output for CLI, also using Chrono crate in core crate to parse dates in metadata

--- rust/portablemc-cli/src/output.rs
+++ rust/portablemc-cli/src/output.rs
@@ -1,9 +1,10 @@
 //! Various utilities to ease outputting human or machine readable text.
 
-use std::fmt::{Display, Write as _};
-use std::time::{Duration, Instant};
-use std::io::{IsTerminal, Write};
+use std::io::{IsTerminal, StdoutLock, Write};
+use std::fmt::{self, Display, Write as _};
 use std::{env, io};
+
+use chrono::TimeDelta;
 
 
 /// An abstraction for outputting to any format on stdout, the goal is to provide an
@@ -49,7 +50,7 @@ impl Output {
     }
 
     /// Enter log mode, this is exclusive with other modes.
-    pub fn log(&mut self) -> LogOutput {
+    pub fn log(&mut self) -> LogOutput<'_> {
 
         // Save the initial cursor position for the first line to be written.
         if self.escape_cursor_cap {
@@ -63,8 +64,16 @@ impl Output {
     }
 
     /// Enter table mode, this is exclusive with other modes.
-    pub fn table(&mut self) -> () {
-        todo!()
+    pub fn table(&mut self, columns: usize) -> TableOutput<'_> {
+        assert_ne!(columns, 0);
+        TableOutput {
+            output: self,
+            writer: io::stdout().lock(),
+            columns,
+            column: 0,
+            buffer: String::new(),
+            cells: Vec::new(),
+        }
     }
 
 }
@@ -308,61 +317,171 @@ pub enum LogLevel {
     Error,
 }
 
-
-/// A common download handler to compute various metrics.
+/// The output table mode, used to build a table.
 #[derive(Debug)]
-pub struct DownloadTracker {
-    /// If a download is running, this contains the instant it started, for speed calc.
-    download_start: Option<Instant>,
+pub struct TableOutput<'a> {
+    /// Exclusive access to output.
+    output: &'a mut Output,
+    /// Locked stdout.
+    writer: StdoutLock<'static>,
+    /// Number of columns.
+    columns: usize,
+    /// The current column being written.
+    column: usize,
+    /// This buffer contains all rendered cells. For human-readable only.
+    buffer: String,
+    /// For each cell, ordered by column and then by row, containing the index where the
+    /// cell's content ends in the shared buffer. For human-readable only.
+    cells: Vec<usize>,
 }
 
-#[derive(Debug)]
-pub struct DownloadMetrics {
-    /// Elapsed time since download started.
-    pub elapsed: Duration,
-    /// Average speed since download started (bytes/s).
-    pub speed: f32,
-}
+impl<'a> TableOutput<'a> {
 
-impl DownloadTracker {
+    /// Fill the next cell on the right. The given content is considered "raw" and 
+    /// unformatted, it will be displayed by default, as-is in all output modes. To
+    /// format the cell for human-readable output, you can use the returned handle.
+    pub fn cell<D: Display>(&mut self, content: D) -> Cell<'_> {
 
-    pub fn new() -> Self {
-        Self { download_start: None }
-    }
-
-    /// Handle progress of a download, returning some metrics if computable.
-    pub fn handle(&mut self, count: u32, total_count: u32, size: u32, total_size: u32) -> Option<DownloadMetrics> {
-
-        let _ = total_size;
-        
-        if self.download_start.is_none() {
-            self.download_start = Some(Instant::now());
-        }
-
-        if size == 0 {
-            if count == total_count {
-                // If all entries have been downloaded but the weight nothing, reset the
-                // download start. This is possible with zero-sized files or cache mode.
-                self.download_start = None;
+        match self.output.mode {
+            OutputMode::Human { .. } => {
+                write!(self.buffer, "{content}").unwrap();
+                self.cells.push(self.buffer.len());
             }
-            return None;
+            OutputMode::TabSeparated {  } => {
+                if self.column == 0 {
+                    write!(self.writer, "row").unwrap();
+                }
+                write!(self.writer, "\t{content}").unwrap();
+            }
         }
 
-        let elapsed = self.download_start.unwrap().elapsed();
-        let speed = size as f32 / elapsed.as_secs_f32();
-
-        if count == total_count {
-            self.download_start = None;
+        self.column += 1;
+        if self.column == self.columns {
+            self.column = 0;
+            if let OutputMode::TabSeparated {  } = self.output.mode {
+                self.writer.write_all(b"\n").unwrap();
+            }
         }
 
-        Some(DownloadMetrics {
-            elapsed,
-            speed,
-        })
+        Cell {
+            output: &mut *self.output,
+            buffer: &mut self.buffer,
+            cells: &mut self.cells,
+        }
+
+    }
+
+    /// Force going to the next row, event if not all cells have been written in the
+    /// current line.
+    pub fn next_row(&mut self) {
+
+        if let OutputMode::TabSeparated {  } = self.output.mode {
+            if self.column == 0 {
+                write!(self.writer, "row").unwrap();
+            }
+        }
+        
+        for _ in self.column..self.columns {
+            if let OutputMode::TabSeparated {  } = self.output.mode {
+                write!(self.writer, "\t").unwrap();
+            }
+            self.cells.push(self.buffer.len());
+        }
+
+        if let OutputMode::TabSeparated {  } = self.output.mode {
+            self.writer.write_all(b"\n").unwrap();
+        }
+
+        self.column = 0;
 
     }
 
 }
+
+impl Drop for TableOutput<'_> {
+    fn drop(&mut self) {
+        
+        if self.column != 0 {
+            self.next_row();
+        }
+
+        if let OutputMode::Human { .. } = self.output.mode {
+
+            let mut columns_width = vec![0usize; self.columns];
+
+            // Initially compute maximum width of each column.
+            let mut column = 0;
+            let mut last_idx = 0;
+            for idx in self.cells.iter().copied() {
+
+                columns_width[column] = columns_width[column].max(idx - last_idx);
+                last_idx = idx;
+                
+                column += 1;
+                if column == self.columns {
+                    column = 0;
+                }
+
+            }
+
+            // Reset and restart again to print.
+            column = 0;
+            last_idx = 0;
+            for idx in self.cells.iter().copied() {
+
+                if column != 0 {
+                    write!(self.writer, " │ ").unwrap();
+                }
+
+                let content = &self.buffer[last_idx..idx];
+                last_idx = idx;
+
+                let width = columns_width[column];
+                write!(self.writer, "{content:width$}").unwrap();
+
+                column += 1;
+                if column == self.columns {
+                    column = 0;
+                    self.writer.write_all(b"\n").unwrap();
+                }
+
+            }
+
+        }
+
+    }
+}
+
+/// A handle for customizing metadata and human-readable format.
+#[derive(Debug)]
+pub struct Cell<'a> {
+    output: &'a mut Output,
+    buffer: &'a mut String,
+    cells: &'a mut Vec<usize>,
+}
+
+impl Cell<'_> {
+
+    /// Format this cell differently from the raw data, only for human-readable.
+    /// Calling this twice will overwrite the first format.
+    pub fn format<D: Display>(&mut self, message: D) -> &mut Self {
+        
+        if let OutputMode::Human { .. } = self.output.mode {
+            // We pop the last cell because it can, and should only be this one.
+            self.cells.pop().unwrap();
+            // Truncate the old cell's content.
+            self.buffer.truncate(self.cells.last().copied().unwrap_or(0));
+            // Rewrite the content.
+            write!(self.buffer, "{message}").unwrap();
+            self.cells.push(self.buffer.len());
+        }
+
+        self
+
+    }
+
+}
+
 
 /// Find the SI unit of a given number and return the number scaled down to that unit.
 pub fn number_si_unit(num: f32) -> (f32, char) {
@@ -371,5 +490,43 @@ pub fn number_si_unit(num: f32) -> (f32, char) {
         ..=999_999.0 => (num / 1_000.0, 'k'),
         ..=999_999_999.0 => (num / 1_000_000.0, 'M'),
         _ => (num / 1_000_000_000.0, 'G'),
+    }
+}
+
+#[derive(Debug)]
+pub struct TimeDeltaDisplay(pub TimeDelta);
+
+impl fmt::Display for TimeDeltaDisplay {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        
+        let years = self.0.num_days() / 365;
+        if years > 0 {
+            return write!(f, "{years} years ago");
+        }
+        
+        // All of this is really wrong but it gives a good, human-friendly, idea.
+        let months = self.0.num_weeks() / 4;
+        if months > 0 {
+            return write!(f, "{months} months ago");
+        }
+        
+        let weeks = self.0.num_weeks();
+        if weeks > 0 {
+            return write!(f, "{weeks} weeks ago");
+        }
+
+        let days = self.0.num_days();
+        if days > 0 {
+            return write!(f, "{days} days ago");
+        }
+
+        let hours = self.0.num_hours();
+        if hours > 0 {
+            return write!(f, "{hours} hours ago");
+        }
+
+        let minutes = self.0.num_minutes();
+        write!(f, "{minutes} minutes ago")
+
     }
 }
