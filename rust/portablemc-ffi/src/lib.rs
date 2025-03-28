0ac56d0fabcb79f31da213bc1d9adec2a0ab86da
Theo Rozier <contact@theorozier.fr>
2025-03-28 22:38:46 +0100

    Working on FFI and defined MSRV

--- rust/portablemc-ffi/src/lib.rs
+++ rust/portablemc-ffi/src/lib.rs
@@ -1,36 +1,3 @@
-use std::ffi::{c_char, CStr, OsStr};
-use std::ptr;
+//! C FFI bindings for PortableMC.
 
-use portablemc::{standard, mojang, fabric, forge};
-
-
-/// The abstract `pmc_inst` type used for pointers in the FFI.
-enum Installer {
-    Standard(standard::Installer),
-    Mojang(mojang::Installer),
-    Fabric(fabric::Installer),
-    Forge(forge::Installer),
-}
-
-extern "C" fn pmc_standard_new(version: *const c_char, main_dir: *const c_char) -> *mut Installer {
-
-    let version = unsafe { CStr::from_ptr(version) };
-    let version = match version.to_str() {
-        Ok(s) => s,
-        Err(_e) => {
-            return ptr::null_mut();
-        }
-    };
-
-    let main_dir = if main_dir.is_null() { 
-        None 
-    } else { 
-        Some(unsafe { CStr::from_ptr(main_dir) })
-    };
-
-    // let inst = standard::Installer::new(version, main_dir)
-    // let inst = Box::new(Installer::Standard())
-
-    todo!()
-
-}
+mod standard;
