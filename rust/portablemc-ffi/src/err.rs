d1f1f4e2776c705c706cb7154e21b9df6c929ca7
Theo Rozier <contact@theorozier.fr>
2025-05-05 19:18:33 +0200

    Working on FFI

--- rust/portablemc-ffi/src/err.rs
+++ rust/portablemc-ffi/src/err.rs
@@ -1,70 +1,51 @@
-//! Error handling for C.
+//! Utilities for easier error handling around the `raw::pmc_err` type.
 
-use std::ffi::{c_char, c_void};
-use std::error::Error;
+use std::ffi::CStr;
+use std::pin::Pin;
 use std::ptr;
 
-use crate::alloc::{extern_box, extern_box_drop_unchecked, extern_box_cstr_from_fmt};
+use crate::alloc::{extern_box, extern_box_drop_unchecked};
+use crate::raw;
 
 
-/// Error codes definitions, shared with C define macros.
-pub mod code {
-
-    pub const INTERNAL: u8                      = 0x01;
-
-    pub const MSA_AUTH_DECLINED: u8             = 0x10;
-    pub const MSA_AUTH_TIMED_OUT: u8            = 0x11;
-    pub const MSA_AUTH_OUTDATED_TOKEN: u8       = 0x12;
-    pub const MSA_AUTH_DOES_NOT_OWN_GAME: u8    = 0x13;
-    pub const MSA_AUTH_INVALID_STATUS: u8       = 0x14;
-    pub const MSA_AUTH_UNKNOWN: u8              = 0x15;
-
-    pub const MSA_DATABASE_IO: u8               = 0x20;
-    pub const MSA_DATABASE_CORRUPTED: u8        = 0x21;
-    pub const MSA_DATABASE_WRITE_FAILED: u8     = 0x22;
-    
-    pub const STANDARD_HIERARCHY_LOOP: u8       = 0x30;
-    pub const STANDARD_VERSION_NOT_FOUND: u8    = 0x31;
-    pub const STANDARD_ASSETS_NOT_FOUND: u8     = 0x32;
-    pub const STANDARD_CLIENT_NOT_FOUND: u8     = 0x33;
-    pub const STANDARD_LIBRARY_NOT_FOUND: u8    = 0x34;
-    pub const STANDARD_JVM_NOT_FOUND: u8        = 0x35;
-    pub const STANDARD_MAIN_CLASS_NOT_FOUND: u8 = 0x36;
-    pub const STANDARD_DOWNLOAD_RESOURCES_CANCELLED: u8 = 0x37;
-    pub const STANDARD_DOWNLOAD: u8             = 0x38;
-
-}
-
-/// A trait to implement on all [`Error`] implementors that also can be exposed to C.
-pub trait ExposedError: Error {
-
-    /// Get a code defined for this exposed error.
-    fn code(&self) -> u8;
-
-    /// If the exposed error provides an additional data that should be given to the user
-    /// through, this function returns that extern data. Note that this function is 
-    /// responsible for allocating an extern box type that can be later freed using
-    /// `pmc_free`.
-    fn extern_data(&self) -> *mut () {
-        ptr::null_mut()
-    }
-
-}
-
-/// Allocate an extern box (see [`crate::alloc::extern_box`]) that contains the given 
-/// error type, returning a pointer to the describing structure.
 #[inline]
-pub fn extern_err<E: ExposedError + 'static>(err: E) -> *mut Err {
-    extern_box(Err { inner: Box::new(err) })
+pub fn extern_err_static(tag: raw::pmc_err_tag, data: raw::pmc_err_data, message: &'static CStr) -> *mut raw::pmc_err {
+    extern_box(raw::pmc_err {
+        tag,
+        data,
+        message: message.as_ptr(),
+    })
+}
+
+#[inline]
+pub fn extern_err(tag: raw::pmc_err_tag, data: raw::pmc_err_data, message: String) -> *mut raw::pmc_err {
+    
+    let owned_message = Pin::new(crate::ensure_nul_terminated(message));
+    
+    extern_box(ExternErr {
+        inner: raw::pmc_err {
+            tag,
+            data,
+            message: owned_message.as_ptr(),
+        },
+        owned_message,
+        owned: (),
+    })
+
+}
+
+/// A trait to bundle an error into an extern `pmc_err` allocated object.
+pub trait IntoExternErr {
+    fn into(self) -> *mut raw::pmc_err;
 }
 
 /// If this result is an error, then the error is extracted and moved into an extern
 /// error, using [`extern_err`], and written in the pointer. Note that if the pointer
 /// of the error is not null, then it is freed anyway, error or not.
 #[inline]
-pub fn extern_err_with<T, E, F>(err_ptr: *mut *mut Err, func: F) -> Result<T, ()>
+pub fn extern_err_with<T, E, F>(err_ptr: *mut *mut raw::pmc_err, func: F) -> Result<T, ()>
 where
-    E: ExposedError + 'static,
+    E: IntoExternErr,
     F: FnOnce() -> Result<T, E>,
 {
 
@@ -85,36 +66,17 @@ where
         Err(err) => {
             // SAFETY: Write the extern error's pointer we just allocated. We are 
             // replacing the null pointer we stored above.
-            unsafe { err_ptr.write(extern_err(err)); }
+            unsafe { err_ptr.write(err.into()); }
             Err(())
         }
     }
     
 }
 
-/// The opaque `pmc_err` type.
-pub struct Err {
-    inner: Box<dyn ExposedError>,
-}
-
-// =======
-// Bindings
-// =======
-
-#[no_mangle]
-pub unsafe extern "C" fn pmc_err_code(err: *const Err) -> u8 {
-    let err = unsafe { &*err };
-    err.inner.code()
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn pmc_err_data(err: *const Err) -> *mut c_void {
-    let err = unsafe { &*err };
-    err.inner.extern_data().cast()
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn pmc_err_message(err: *const Err) -> *mut c_char {
-    let err = unsafe { &*err };
-    extern_box_cstr_from_fmt(format_args!("{}", err.inner))
+/// Implementation of the `pmc_err` type.
+#[repr(C)]
+struct ExternErr<O> {
+    inner: raw::pmc_err,
+    owned_message: Pin<Box<[u8]>>,
+    owned: O,
 }
